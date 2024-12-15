bc70ad239d5ea44bbdad1007245fabfc5b68385d
kirillmorgunov <kirillmorgunov.mail@gmail.com>
2024-12-15 18:55:53 +0400

    add deploy file

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: 'idea-test',
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
