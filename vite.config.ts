552bf726358383ab5f6741710817da46bf900130
kirillmorgunov <kirillmorgunov.mail@gmail.com>
2024-12-15 19:06:20 +0400

    fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: 'idea-test',
+  base: '/idea',
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
